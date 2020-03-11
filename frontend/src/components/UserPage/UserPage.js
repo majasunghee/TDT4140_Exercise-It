a47ef9ec4eb4e3396b1ace7dc701fc977371172f
Aleksander Westergaard Karlsen <aleksawk@stud.ntnu.no>
2020-03-11 17:09:12 +0100

    #14 started work on the userpage, this is just a template

---
+++ frontend/src/components/UserPage/UserPage.js
@@ -0,0 +1,33 @@
+import React from "react";
+import styles from "../../App.module.css";
+
+
+class UserPage extends React.Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            
+        };
+    }
+
+    render(){
+        if(
+            this.state == "ost"
+        ){
+            return (
+                <div>
+                    
+                </div>  
+            );
+        }
+    }
+
+
+
+    
+
+
+
+
+}
