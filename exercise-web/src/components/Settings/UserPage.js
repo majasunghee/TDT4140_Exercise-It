e556f34b75f2978185cc79956e903f89bc11f252
Aleksander Westergaard Karlsen <aleksawk@stud.ntnu.no>
2020-03-23 15:41:04 +0100

    #14 la til profil på brukerside

--- exercise-web/src/components/Settings/UserPage.js
+++ exercise-web/src/components/Settings/UserPage.js
@@ -1,13 +1,30 @@
 import React from "react";
 import styles from "./settings.module.css";
 
+
 const UserPage = props => {
   return (
     <div className={styles.container}>
       <div className={styles.infoHeader}>
-        <div className={styles.mainHeader}>{props.username}</div>
-        <br />
-        <p>Her skal det inn mange treningsøkter for denne brukeren</p>
+        <div className={styles.UserContainer}>
+          <div className={styles.PB_frame}>
+          </div>
+          <div className={styles.personalia}>
+          <p id={styles.Name}>Aleksander</p>
+            <p>Profesjonell</p>
+          </div>
+          <div className={styles.statWrapper}>
+            <div> 
+              <p id={styles.stat}>154</p>
+              <p id={styles.Hstat}>Innlegg</p>
+            </div>
+            <div>
+              <p id={styles.stat}>14</p>
+              <p id={styles.Hstat}>Mål fullført</p>
+            </div>
+          </div>
+
+        </div>
       </div>
     </div>
   );
