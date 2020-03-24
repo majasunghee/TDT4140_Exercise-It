aaa1385c088ef16fe20d179e5d947a592246cfb0
Aleksander Westergaard Karlsen <aleksawk@stud.ntnu.no>
2020-03-24 12:06:26 +0100

    #14 splitte innlegg inn i økter og øvelser

--- exercise-web/src/components/Settings/UserPage.js
+++ exercise-web/src/components/Settings/UserPage.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styles from "./settings.module.css";
 
-
 const UserPage = props => {
   return (
     <div className={styles.container}>
@@ -10,25 +9,41 @@ const UserPage = props => {
           <div className={styles.PB_frame}>
           </div>
           <div className={styles.personalia}>
-          <p id={styles.Name}>Aleksander</p>
-            <p>Profesjonell</p>
+            <p id={styles.Name}>
+              Aleksander
+            </p>
+            <p>
+              Profesjonell
+            </p>
           </div>
           <div className={styles.statWrapper}>
             <div> 
-              <p id={styles.stat}>154</p>
-              <p id={styles.Hstat}>Øvelser</p>
+              <p id={styles.stat}>
+                25
+              </p>
+              <p id={styles.Hstat}>
+                Øvelser
+              </p>
             </div>
             <div>
-              <p id={styles.stat}>14</p>
-              <p id={styles.Hstat}>Økter</p>
+              <p id={styles.stat}>
+                14
+              </p>
+              <p id={styles.Hstat}>
+                Økter
+              </p>
             </div>
-            
           </div>
           <div className={styles.ContentSplitterWrapper}>
-              <p>Innlegg</p>
+              <p>Øvelser</p>
               <div className={styles.linje}></div>
-            </div>
-        </div>
+          </div>
+          <div className={styles.ContentSplitterWrapper}>
+              <p>Økter</p>
+              <div className={styles.linje}></div>
+          </div>
+          </div>
+          
       </div>
     </div>
   );
