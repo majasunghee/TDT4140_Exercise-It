3505457634ee7b3fb96ce1cf4d1123162fbb9443
Jonas Sæther <jonas.sath@outlook.com>
2020-04-08 18:34:03 +0200

    mac also auto setup env

--- runserver.sh
+++ runserver.sh
@@ -1,6 +1,6 @@
 cd exercise-api
-virtualenv env || echo "env already installed"
+if [ -d "$env" ]; then echo "env exists"; else python3 -m virtualenv env || python -m virtualenv env; fi
 cd ..
-source exercise-api/env/Scripts/activate || source exercise-api/env/bin/activate
+source exercise-api/env/bin/activate || source exercise-api/env/Scripts/activate
 pip install -r requirements.txt
-python3 manage.py runserver || python manage.py runserver
+python3 manage.py runserver || python manage.py runserver
