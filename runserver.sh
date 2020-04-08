5d240d2ff89c7c0f61436fcd12340f1d56d2ed3d
Jonas Sæther <jonasaet@stud.ntnu.no>
2020-04-08 18:07:40 +0200

    auto env creation

--- runserver.sh
+++ runserver.sh
@@ -1,3 +1,6 @@
+cd exercise-api
+virtualenv env || echo "env already installed"
+cd ..
 source exercise-api/env/Scripts/activate || source exercise-api/env/bin/activate
 pip install -r requirements.txt
-python3 manage.py runserver || python3 manage.py runserver
+python3 manage.py runserver || python manage.py runserver
