5ff5b1ebbf2f0492b353d63d3143a6c684db508d
Jonas Sæther <jonasaet@stud.ntnu.no>
2020-04-08 20:53:44 +0200

    bump

--- runserver.sh
+++ runserver.sh
@@ -3,4 +3,6 @@ if [ -d env/ ]; then echo "env exists"; else virtualenv env || python3 -m virtua
 if [ -d env/bin/ ]; then source env/bin/activate; else source env/Scripts/activate; fi
 cd ..
 pip install -r requirements.txt
-if python3 --version; then python3 manage.py runserver; else python manage.py runserver; fi
+if python3 --version; then python3 manage.py runserver; else 
+if python --version; then python manage.py runserver; else
+if py --version; then py manage.py runserver; else echo "no python"; fi
