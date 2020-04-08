5598f5f2aebfa5fefe4ca04f486212c685aaf6e5
Jonas Sæther <jonas.sath@outlook.com>
2020-04-08 18:01:07 +0200

    auto setup macos

--- runserver.sh
+++ runserver.sh
@@ -1,5 +1,3 @@
-source exercise-api/env/Scripts/activate
-source exercise-api/env/bin/activate
+source exercise-api/env/Scripts/activate || source exercise-api/env/bin/activate
 pip install -r requirements.txt
-python manage.py runserver
-python3 manage.py runserver
+python3 manage.py runserver || python3 manage.py runserver
