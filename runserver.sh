0ff40602b1c8db413ecb54e0d6f5ce9d0b5d8588
jonasaet <jonasaet@stud.ntnu.no>
2020-04-08 12:11:59 -0700

    python fix

--- runserver.sh
+++ runserver.sh
@@ -1,8 +1,8 @@
 cd exercise-api
-if [ -d env/ ]; then echo "env exists"; else virtualenv env || python3 -m virtualenv env; fi
+if [ -d env/ ]; then echo "env exists"; else virtualenv env || python -m virtualenv env || python3 -m virtualenv env || py -m virtualenv env; fi
 if [ -d env/bin/ ]; then source env/bin/activate; else source env/Scripts/activate; fi
 cd ..
 pip install -r requirements.txt
-if python3 --version; then python3 manage.py runserver; else 
-if python --version; then python manage.py runserver; else
-if py --version; then py manage.py runserver; else echo "no python"; fi
+if python3 --version; then python3 manage.py runserver; fi 
+if python --version; then python manage.py runserver; fi
+if py --version; then py manage.py runserver; fi
